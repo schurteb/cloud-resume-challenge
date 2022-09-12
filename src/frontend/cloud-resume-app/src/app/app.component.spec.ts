c772dbb263d36cc97062d9333d896806d4af1960
Benjamin Schurtenberger <benjamin.schurtenberger@gmail.com>
2022-09-12 08:19:54 +0200

    fix(frontend): comment irrelevant test case

--- src/frontend/cloud-resume-app/src/app/app.component.spec.ts
+++ src/frontend/cloud-resume-app/src/app/app.component.spec.ts
@@ -26,10 +26,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('cloud-resume-app');
   });
 
-  it('should render title', () => {
+  /*it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('.content span')?.textContent).toContain('cloud-resume-app app is running!');
-  });
+  });*/
 });
