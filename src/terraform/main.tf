9f1e3fe55f97e8c3b9b6a1e6caa449b31b4b6311
Benjamin Schurtenberger <benjamin.schurtenberger@gmail.com>
2022-09-14 08:09:42 +0200

    feat(terraform): add alias to cloudfront distro

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -43,6 +43,10 @@ resource "aws_cloudfront_distribution" "cloudfront_distribution_public_site" {
   http_version        = "http2"
   is_ipv6_enabled     = "true"
 
+  aliases = [
+    "resume.schurteb.ch"
+  ]
+
   origin {
     domain_name = aws_s3_bucket.schurteb-cloud-resume-challenge-bucket.bucket_regional_domain_name
     origin_id   = aws_s3_bucket.schurteb-cloud-resume-challenge-bucket.bucket_regional_domain_name
