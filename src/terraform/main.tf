e46e4d931e85fea590cb6361e1c9e76ef1e72cef
Schurtenberger Benjamin <benjamin.schurtenberger.ext@siemens.com>
2023-05-19 15:49:26 +0200

    feat(terraform): add origin access control, dynamodb table and lambda function

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -2,7 +2,7 @@ module "pipeline_authentication" {
   source = "./modules/pipeline-authentication"
 
   region = var.region
-  
+
   default_tags = merge(
     var.default_tags,
     tomap({
@@ -22,14 +22,19 @@ resource "aws_s3_bucket" "schurteb-cloud-resume-challenge-bucket" {
   )
 }
 
-resource "aws_s3_bucket_policy" "allow_access_from_cloudfront_oai" {
+resource "aws_s3_bucket_policy" "allow_access_from_cloudfront_oac" {
   bucket = aws_s3_bucket.schurteb-cloud-resume-challenge-bucket.id
-  policy = data.aws_iam_policy_document.allow_access_from_cloudfront_oai.json
+  policy = data.aws_iam_policy_document.allow_access_from_cloudfront_oac.json
 }
 
-resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
-  comment = "The OAI to be used with cloudfront and the s3 bucket for the public site"
+resource "aws_cloudfront_origin_access_control" "origin_access_control" {
+  name                              = "${var.name}-origin-access-control"
+  description                       = "The OAI to be used with cloudfront and the s3 bucket for the public site"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
 }
+
 
 resource "aws_cloudfront_distribution" "cloudfront_distribution_public_site" {
   default_cache_behavior {
@@ -61,12 +66,9 @@ resource "aws_cloudfront_distribution" "cloudfront_distribution_public_site" {
   ]
 
   origin {
-    domain_name = aws_s3_bucket.schurteb-cloud-resume-challenge-bucket.bucket_regional_domain_name
-    origin_id   = aws_s3_bucket.schurteb-cloud-resume-challenge-bucket.bucket_regional_domain_name
-
-    s3_origin_config {
-      origin_access_identity = "origin-access-identity/cloudfront/${aws_cloudfront_origin_access_identity.origin_access_identity.id}"
-    }
+    domain_name              = aws_s3_bucket.schurteb-cloud-resume-challenge-bucket.bucket_regional_domain_name
+    origin_id                = aws_s3_bucket.schurteb-cloud-resume-challenge-bucket.bucket_regional_domain_name
+    origin_access_control_id = aws_cloudfront_origin_access_control.origin_access_control.id
   }
 
   price_class = "PriceClass_100"
@@ -87,8 +89,57 @@ resource "aws_cloudfront_distribution" "cloudfront_distribution_public_site" {
   )
 
   viewer_certificate {
-    minimum_protocol_version       = "TLSv1"
-    acm_certificate_arn = "arn:aws:acm:us-east-1:420018626735:certificate/d6e6e836-cc21-418f-9cfd-b34c496d9965"
-    ssl_support_method = "sni-only"
+    minimum_protocol_version = "TLSv1"
+    acm_certificate_arn      = "arn:aws:acm:us-east-1:420018626735:certificate/d6e6e836-cc21-418f-9cfd-b34c496d9965"
+    ssl_support_method       = "sni-only"
   }
+}
+
+resource "aws_dynamodb_table" "dynamodb_backend" {
+  name         = "${var.name}-backend"
+  hash_key     = "ID"
+  billing_mode = "PAY_PER_REQUEST"
+
+  attribute {
+    name = "ID"
+    type = "S"
+  }
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name               = "${var.name}-lambda-role"
+  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role_policy.json
+}
+
+resource "aws_iam_policy" "lambda_role_permissions_policy" {
+  name   = "${var.name}-site-view-counter-policy"
+  path   = "/"
+  policy = data.aws_iam_policy_document.lambda_role_permissions_policy.json
+
+  lifecycle {
+    create_before_destroy = false
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_role_permissions_policy_attachment" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.lambda_role_permissions_policy.arn
+}
+
+resource "aws_lambda_function" "lambda_site_view_counter" {
+  filename      = "lambda_function_site_view_counter_payload.zip"
+  function_name = "${var.name}-site-view-counter"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "lambda_site_view_counter.lambda_handler"
+  memory_size   = 256
+  timeout       = 10
+
+  source_code_hash = data.archive_file.lambda_site_view_counter_src.output_base64sha256
+
+  runtime = "python3.9"
+}
+
+resource "aws_lambda_function_url" "lambda_site_view_counter_invoke_url" {
+  function_name      = aws_lambda_function.lambda_site_view_counter.function_name
+  authorization_type = "NONE"
 }
