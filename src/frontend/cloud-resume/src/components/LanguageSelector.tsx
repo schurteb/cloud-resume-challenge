382f138ce06f5106c1e385bd912007886978666b
Benjamin Schurtenberger <benjamin.schurtenberger.ext@siemens.com>
2024-04-23 10:34:30 +0200

    style(frontend): fix linting issues in language selector component

--- src/frontend/cloud-resume/src/components/LanguageSelector.tsx
+++ src/frontend/cloud-resume/src/components/LanguageSelector.tsx
@@ -1,77 +1,102 @@
-import * as React from 'react';
-import { Box, FormControl, MenuItem, Select, SelectChangeEvent } from '@mui/material';
-import { useTranslation } from 'react-i18next';
-import ReactCountryFlag, { ReactCountryFlagProps } from 'react-country-flag';
+import * as React from "react";
+import {
+  Box,
+  FormControl,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+} from "@mui/material";
+import { useTranslation } from "react-i18next";
+import ReactCountryFlag, { ReactCountryFlagProps } from "react-country-flag";
+
+const CountryFlag = ({ countryCode, ...props }: ReactCountryFlagProps) => {
+  return (
+    <ReactCountryFlag
+      className="countryFlag"
+      countryCode={countryCode}
+      style={{
+        fontSize: "2em",
+        lineHeight: "2em",
+      }}
+      aria-label="Great Britain"
+      svg
+    />
+  );
+};
 
 export default function LanguageSelector() {
-    const [languageCode, setLanguageCode] = React.useState('en');
-    const { i18n } = useTranslation();
+  const [languageCode, setLanguageCode] = React.useState("en");
+  const { i18n } = useTranslation();
 
-    const handleLanguageChange = (event: SelectChangeEvent<any>, child: React.ReactNode) => {
-      if (event.target.value !== languageCode) {
-        i18n.changeLanguage(event.target.value);
-        setLanguageCode(event.target.value);
-      }
-    };
+  const handleLanguageChange = (
+    event: SelectChangeEvent<any>,
+    child: React.ReactNode
+  ) => {
+    if (event.target.value !== languageCode) {
+      i18n.changeLanguage(event.target.value);
+      setLanguageCode(event.target.value);
+    }
+  };
 
-    const CountryFlag = ({countryCode, ...props}: ReactCountryFlagProps) => <ReactCountryFlag
-    className="countryFlag"
-    countryCode={countryCode}
-    style={{
-        fontSize: '2em',
-        lineHeight: '2em',
-    }}
-    aria-label="Great Britain"
-    svg
-    />
-
-    return (
-      <Box
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        width: "100%",
+        //minWidth: '80px',
+        alignItems: "center",
+        justifyContent: "center",
+        //bgcolor: 'background.default',
+        color: "text.primary",
+        borderRadius: 1,
+        //p: 3,
+      }}
+    >
+      <FormControl
+        size="small"
         sx={{
-          display: 'flex',
-          width: '100%',
-          //minWidth: '80px',
-          alignItems: 'center',
-          justifyContent: 'center',
-          //bgcolor: 'background.default',
-          color: 'text.primary',
-          borderRadius: 1,
-          //p: 3,
+          border: "0px",
         }}
       >
-        <FormControl  size='small' sx={{
-              border: '0px'
-            }}>
-          <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            value={languageCode}
-            onChange={handleLanguageChange}
-            sx={{
-              border: '0px',
-              boxShadow: 'none',
-              '.MuiOutlinedInput-notchedOutline': { border: 0, borderColor: 'transparent'},
-              '&.Mui-focused .MuiOutlinedInput-notchedOutline': { border: 0, borderColor: 'transparent' },
-              '&.Mui-hovered .MuiOutlinedInput-notchedOutline': { border: 0, borderColor: 'transparent' }
-            }}
-          >
-            <MenuItem value={'en'}>
-              <CountryFlag countryCode='gb' />
-            </MenuItem>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={languageCode}
+          onChange={handleLanguageChange}
+          sx={{
+            border: "0px",
+            boxShadow: "none",
+            ".MuiOutlinedInput-notchedOutline": {
+              border: 0,
+              borderColor: "transparent",
+            },
+            "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+              border: 0,
+              borderColor: "transparent",
+            },
+            "&.Mui-hovered .MuiOutlinedInput-notchedOutline": {
+              border: 0,
+              borderColor: "transparent",
+            },
+          }}
+        >
+          <MenuItem value={"en"}>
+            <CountryFlag countryCode="gb" />
+          </MenuItem>
 
-            <MenuItem value={'de'}>
-              <CountryFlag countryCode='de' />
-            </MenuItem>
+          <MenuItem value={"de"}>
+            <CountryFlag countryCode="de" />
+          </MenuItem>
 
-            {/*<MenuItem value={'fr'}>
+          {/*<MenuItem value={'fr'}>
               <CountryFlag countryCode='fr' />
             </MenuItem>
 
             <MenuItem value={'it'}>
               <CountryFlag countryCode='it' />
             </MenuItem>*/}
-          </Select>
-        </FormControl>
-      </Box>
-    );
-  }
+        </Select>
+      </FormControl>
+    </Box>
+  );
+}
